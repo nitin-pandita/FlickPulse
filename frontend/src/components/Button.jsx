8e91260ef548d6fc9f78185f0abde12932cb006d
nitin-pandita <nitin_207013@saitm.org>
2023-09-11 11:47:53 +0530

    Customized the button

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import { AiOutlineArrowDown } from "react-icons/ai";
 const Button = () => {
-  return <div>Button</div>;
+  return (
+    <div>
+      <button
+        className="
+        bg-transparent
+        border-2
+        border-white
+        text-white
+        font-bold
+        py-4
+        px-6
+        rounded-full
+        hover:bg-white
+        hover:text-black
+        transition
+        duration-300
+        ease-in-out
+        transform
+        hover:-translate-y-1
+        hover:scale-110
+        mt-5
+        
+        "
+      >
+        <AiOutlineArrowDown />
+      </button>
+    </div>
+  );
 };
 
 export default Button;
